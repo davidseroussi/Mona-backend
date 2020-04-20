0eeede3a1692c4307b98a1db2edb5e69b9194344
David Seroussi <davidseroussi97@gmail.com>
2020-04-20 15:25:20 +0100

    Added support for multiple event parameters

--- microservices/backend/core/requestHandler.js
+++ microservices/backend/core/requestHandler.js
@@ -6,27 +6,44 @@ const emptyWhenNull = R.when(R.isNil, (_) => '""');
 
 const parseBody = R.pipe(R.prop("body"), JSON.parse);
 
-const handle = (fn) =>
-    R.pipe(
+const queriedParams = (event, props) => {
+
+    return R.merge(
         R.cond([
             [R.propEq("httpMethod", "POST"), parseBody],
             [R.propEq("httpMethod", "PATCH"), parseBody],
             [R.propEq("httpMethod", "GET"), R.prop("queryStringParameters")],
             [R.T, R.always(null)],
-        ]),
-        R.tap(console.log),
-        emptyWhenNull,
-        fn,
-        (x) => Promise.resolve(x),
-        R.then(R.tap(console.log)),
-        R.then(bodyStringifyer),
-        R.then(R.identity),
-        R.otherwise((err) => {
+        ])(event),
+        R.unless(R.isNil, (x) => R.reduce(R.merge, {}, R.props(x)(event)))(props)
+    )
+}
+
+const handle = (fn, params) =>
+    R.tryCatch(
+        R.pipe(
+            (event) => queriedParams(event, params),
+            R.tap(console.log),
+            emptyWhenNull,
+            fn,
+            (x) => Promise.resolve(x),
+            R.then(R.tap(console.log)),
+            R.then(bodyStringifyer),
+            R.then(R.identity),
+            R.otherwise((err) => {
+                console.log(err);
+                return {
+                    statusCode: 500,
+                };
+            })
+        ),
+        (err, val) => {
             console.log(err);
-            return {
-                statusCode: 500,
-            };
-        })
-    );
+            return R.always({
+                statusCode: 500
+            })
+        }
+
+    )
 
 module.exports = { handle };
