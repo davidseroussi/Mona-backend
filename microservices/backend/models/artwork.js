efe7daf8e2677c267284172d273f0defdc6adb1d
David Seroussi <davidseroussi97@gmail.com>
2020-04-22 12:01:17 +0100

    Added xo linting

--- microservices/backend/models/artwork.js
+++ microservices/backend/models/artwork.js
@@ -1,23 +1,25 @@
-const mongoose = require("mongoose");
-const ratingSubSchema = require("./sub_rating");
+const mongoose = require('mongoose');
+const ratingSubSchema = require('./sub-rating');
 
 const artworkSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    title: {
-        type: String,
-        required: [true, "Artwork title is required"],
-    },
-    description: String,
-    audioFileUrl: String,
-    imageUrl: String,
-    rating: ratingSubSchema,
-    exhibitionId: mongoose.Schema.Types.ObjectId,
+	title: {
+		type: String,
+		required: [true, 'Artwork title is required']
+	},
+	description: String,
+	audioFileUrl: String,
+	imageUrl: String,
+	rating: ratingSubSchema,
+	exhibition: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Exhibition'
+	}
 });
 
 artworkSchema.methods.toJSON = function () {
-    var obj = this.toObject();
-    delete obj.__v;
-    return obj;
+	const object = this.toObject();
+	delete object.__v;
+	return object;
 };
 
-module.exports = mongoose.model("Artwork", artworkSchema);
+module.exports = mongoose.model('Artwork', artworkSchema);
--- microservices/backend/models/exhibition.js
+++ microservices/backend/models/exhibition.js
@@ -1,68 +1,68 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const ratingSubSchema = require("./sub_rating");
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+const ratingSubSchema = require('./sub-rating');
 
 const exhibitionSchema = new Schema({
-    title: {
-        type: String,
-        required: [true, "Title is missing"],
-    },
-    description: String,
-    imageUrl: String,
-    rates: [
-        {
-            price: {
-                type: Number,
-                min: [0, "Price cannot be negative"],
-            },
-            category: {
-                type: String,
-                //TODO Remplacer par un objet à part entière
-                enum: [
-                    "Baby",
-                    "Child",
-                    "Teen",
-                    "Student",
-                    "Adult",
-                    "PriorityPerson",
-                    "Standard",
-                ],
-            },
-        },
-    ],
-    opening: {
-        isOpen: Boolean,
-        startDate: Date,
-        endDate: Date,
-        openingDays: [Number],
-    },
-    artworks: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Artwork",
-        },
-    ],
-    rating: ratingSubSchema,
-    visits: {
-        visitsCount: {
-            type: Number,
-            min: [0, "Number of visits cannot be negative"],
-        },
-        ticketsCount: {
-            type: Number,
-            min: [0, "Number of tickets sold cannot be negative"],
-        },
-        sales: {
-            type: Number,
-            min: [0, "Sales cannot be negative"],
-        },
-    },
+	title: {
+		type: String,
+		required: [true, 'Title is missing']
+	},
+	description: String,
+	imageUrl: String,
+	rates: [
+		{
+			price: {
+				type: Number,
+				min: [0, 'Price cannot be negative']
+			},
+			category: {
+				type: String,
+				enum: [
+					'Baby',
+					'Child',
+					'Teen',
+					'Student',
+					'Adult',
+					'PriorityPerson',
+					'Standard'
+				]
+			}
+		}
+	],
+	opening: {
+		isOpen: Boolean,
+		startDate: Date,
+		endDate: Date,
+		openingDays: [Number]
+	},
+	artworks: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Artwork'
+		}
+	],
+	rating: ratingSubSchema,
+	visits: {
+		visitsCount: {
+			type: Number,
+			min: [0, 'Number of visits cannot be negative']
+		},
+		ticketsCount: {
+			type: Number,
+			min: [0, 'Number of tickets sold cannot be negative']
+		},
+		sales: {
+			type: Number,
+			min: [0, 'Sales cannot be negative']
+		}
+	}
 });
 
 exhibitionSchema.methods.toJSON = function () {
-    var obj = this.toObject();
-    delete obj.__v;
-    return obj;
+	const object = this.toObject();
+	delete object.__v;
+	return object;
 };
 
-module.exports = mongoose.model("Exhibition", exhibitionSchema);
+module.exports = mongoose.model('Exhibition', exhibitionSchema);
