175650cf12c1c065b6bf38e8da9b234da46dfde9
David Seroussi <davidseroussi97@gmail.com>
2020-04-22 12:40:22 +0100

    Added david's cluster to config

--- microservices/backend/core/database.js
+++ microservices/backend/core/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config-yml');
 
 const connect = () => {
-	mongoose.connect(config.db_local.uri, {
+	mongoose.connect(config.db_david.uri, {
 		useNewUrlParser: true
 		// UseUnifiedTopology: true,
 	});
